107a24e74a041fdba236dfae273c5e6713673acc
hard2want <thedukeofchicago@gmail.com>
2018-06-05 13:58:55 -0500

    first commit via CLI

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by The Duke on 6/5/18.
+//  Copyright © 2018 The Duke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
